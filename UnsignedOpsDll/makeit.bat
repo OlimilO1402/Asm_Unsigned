f1a16290ad25dc3a8f3498a16fd326e3a70c9264
OlimilO1402 <olimilo@gmx.net>
2025-08-03 08:14:59 +0200

    update to makeit

--- UnsignedOpsDll/makeit.bat
+++ UnsignedOpsDll/makeit.bat
@@ -1,15 +1,15 @@
 @echo off
-if exist C:\Masm32Projects\UnsignedOps\UnsignedOps.obj del C:\Masm32Projects\UnsignedOps\UnsignedOps.obj
-if exist C:\Masm32Projects\UnsignedOps\UnsignedOps.dll del C:\Masm32Projects\UnsignedOps\UnsignedOps.dll
-copy "\\SOLS_DS\Daten\GitHubRepos\VB\Asm_Unsigned\UnsignedOpsDll\UnsignedOps.asm" "C:\Masm32Projects\UnsignedOps\UnsignedOps.asm"
-copy "\\SOLS_DS\Daten\GitHubRepos\VB\Asm_Unsigned\UnsignedOpsDll\UnsignedOps.def" "C:\Masm32Projects\UnsignedOps\UnsignedOps.def"
-cd C:\Masm32Projects\UnsignedOps\
-C:\Masm32Projects\UnsignedOps\makeit.bat
-rem C:\masm32\bin\ml /c /coff C:\Masm32Projects\UnsignedOps\UnsignedOps.asm
-rem C:\masm32\bin\Link /SUBSYSTEM:WINDOWS /DLL /DEF:C:\Masm32Projects\UnsignedOps\UnsignedOps.def C:\Masm32Projects\UnsignedOps\UnsignedOps.obj 
-copy "C:\Masm32Projects\UnsignedOps\UnsignedOps.dll" "\\SOLS_DS\Daten\GitHubRepos\VB\Asm_Unsigned\UnsignedOpsDll\UnsignedOps.dll"
-copy "C:\Masm32Projects\UnsignedOps\UnsignedOps.lib" "\\SOLS_DS\Daten\GitHubRepos\VB\Asm_Unsigned\UnsignedOpsDll\UnsignedOps.lib"
-del C:\Masm32Projects\UnsignedOps\UnsignedOps.obj
-del C:\Masm32Projects\UnsignedOps\UnsignedOps.exp
-dir C:\Masm32Projects\UnsignedOps\UnsignedOps.*
+if exist C:\MASM\Projects\UnsignedOps\UnsignedOps.obj del C:\MASM\Projects\UnsignedOps\UnsignedOps.obj
+if exist C:\MASM\Projects\UnsignedOps\UnsignedOps.dll del C:\MASM\Projects\UnsignedOps\UnsignedOps.dll
+copy "\\SOLS_DS\Daten\GitHubRepos\VB\Asm_Unsigned\UnsignedOpsDll\UnsignedOps.asm" "C:\MASM\Projects\UnsignedOps\UnsignedOps.asm"
+copy "\\SOLS_DS\Daten\GitHubRepos\VB\Asm_Unsigned\UnsignedOpsDll\UnsignedOps.def" "C:\MASM\Projects\UnsignedOps\UnsignedOps.def"
+cd C:\MASM\Projects\UnsignedOps\
+C:\MASM\Projects\UnsignedOps\makeit.bat
+rem C:\MASM\x86\ml /c /coff C:\Masm32Projects\UnsignedOps\UnsignedOps.asm
+rem C:\MASM\x86\link /SUBSYSTEM:WINDOWS /DLL /DEF:C:\MASM\Projects\UnsignedOps\UnsignedOps.def C:\MASM\Projects\UnsignedOps\UnsignedOps.obj 
+copy "C:\MASM\Projects\UnsignedOps\UnsignedOps.dll" "\\SOLS_DS\Daten\GitHubRepos\VB\Asm_Unsigned\UnsignedOpsDll\UnsignedOps.dll"
+copy "C:\MASM\Projects\UnsignedOps\UnsignedOps.lib" "\\SOLS_DS\Daten\GitHubRepos\VB\Asm_Unsigned\UnsignedOpsDll\UnsignedOps.lib"
+del C:\MASM\Projects\UnsignedOps\UnsignedOps.obj
+del C:\MASM\Projects\UnsignedOps\UnsignedOps.exp
+dir C:\MASM\Projects\UnsignedOps\UnsignedOps.*
 pause
